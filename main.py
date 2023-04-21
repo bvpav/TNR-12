73c7a879ccccf71802d6a7a99edf16006166584f
bvpav <bvpav@tutanota.com>
2023-04-21 08:59:19 +0300

    finalized motor code

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import evdev
 
 from gamepad import Gamepad
+import motor
 
 CONTROLLER_MAC = '4C:B9:9B:05:D2:83'
 
@@ -13,219 +14,51 @@ def get_ps5_device():
     raise LookupError('Unable to find PS5 controller. Have you paired it?')
 
 
-device = get_ps5_device()
-gamepad = Gamepad()
-
-
-@gamepad.on_square_down
-def handle_square_down(*args):
-    print('on_square_down', *args)
-
-
-@gamepad.on_square_up
-def handle_square_up(*args):
-    print('on_square_up', *args)
-
-
-@gamepad.on_x_down
-def handle_x_down(*args):
-    print('on_x_down', *args)
-
-
-@gamepad.on_x_up
-def handle_x_up(*args):
-    print('on_x_up', *args)
-
-
-@gamepad.on_circle_down
-def handle_circle_down(*args):
-    print('on_circle_down', *args)
-
-
-@gamepad.on_circle_up
-def handle_circle_up(*args):
-    print('on_circle_up', *args)
-
-
-@gamepad.on_triangle_down
-def handle_triangle_down(*args):
-    print('on_triangle_down', *args)
-
-
-@gamepad.on_triangle_up
-def handle_triangle_up(*args):
-    print('on_triangle_up', *args)
-
-
-@gamepad.on_l1_down
-def handle_l1_down(*args):
-    print('on_l1_down', *args)
-
-
-@gamepad.on_l1_up
-def handle_l1_up(*args):
-    print('on_l1_up', *args)
-
-
-@gamepad.on_r1_down
-def handle_r1_down(*args):
-    print('on_r1_down', *args)
-
-
-@gamepad.on_r1_up
-def handle_r1_up(*args):
-    print('on_r1_up', *args)
-
-
-@gamepad.on_l2_down
-def handle_l2_down(*args):
-    print('on_l2_down', *args)
-
-
-@gamepad.on_l2_up
-def handle_l2_up(*args):
-    print('on_l2_up', *args)
-
-
-@gamepad.on_r2_down
-def handle_r2_down(*args):
-    print('on_r2_down', *args)
-
-
-@gamepad.on_r2_up
-def handle_r2_up(*args):
-    print('on_r2_up', *args)
-
-
-@gamepad.on_share_down
-def handle_share_down(*args):
-    print('on_share_down', *args)
-
-
-@gamepad.on_share_up
-def handle_share_up(*args):
-    print('on_share_up', *args)
-
-
-@gamepad.on_pause_down
-def handle_pause_down(*args):
-    print('on_pause_down', *args)
-
-
-@gamepad.on_pause_up
-def handle_pause_up(*args):
-    print('on_pause_up', *args)
-
-
-@gamepad.on_l3_down
-def handle_l3_down(*args):
-    print('on_l3_down', *args)
-
-
-@gamepad.on_l3_up
-def handle_l3_up(*args):
-    print('on_l3_up', *args)
-
-
-@gamepad.on_r3_down
-def handle_r3_down(*args):
-    print('on_r3_down', *args)
-
-
-@gamepad.on_r3_up
-def handle_r3_up(*args):
-    print('on_r3_up', *args)
-
-
-@gamepad.on_playstation_down
-def handle_playstation_down(*args):
-    print('on_playstation_down', *args)
-
-
-@gamepad.on_playstation_up
-def handle_playstation_up(*args):
-    print('on_playstation_up', *args)
-
-
-@gamepad.on_touchpad_down
-def handle_touchpad_down(*args):
-    print('on_touchpad_down', *args)
-
-
-@gamepad.on_touchpad_up
-def handle_touchpad_up(*args):
-    print('on_touchpad_up', *args)
-
-
-@gamepad.on_joy_l_x
-def handle_joy_l_x(*args):
-    print('on_joy_l_x', *args)
-
-
-@gamepad.on_joy_l_y
-def handle_joy_l_y(*args):
-    print('on_joy_l_y', *args)
-
-
-@gamepad.on_joy_r_x
-def handle_joy_r_x(*args):
-    print('on_joy_r_x', *args)
-
-
-@gamepad.on_l2_analog
-def handle_l2_analog(*args):
-    print('on_l2_analog', *args)
-
-
-@gamepad.on_r2_analog
-def handle_r2_analog(*args):
-    print('on_r2_analog', *args)
-
-
-@gamepad.on_joy_r_y
-def handle_joy_r_y(*args):
-    print('on_joy_r_y', *args)
-
-
-@gamepad.on_leftpad_left_down
-def handle_leftpad_left_down(*args):
-    print('on_leftpad_left_down', *args)
-
-
-@gamepad.on_leftpad_left_up
-def handle_leftpad_left_up(*args):
-    print('on_leftpad_left_up', *args)
-
-
-@gamepad.on_leftpad_right_down
-def handle_leftpad_right_down(*args):
-    print('on_leftpad_right_down', *args)
-
-
-@gamepad.on_leftpad_right_up
-def handle_leftpad_right_up(*args):
-    print('on_leftpad_right_up', *args)
-
-
-@gamepad.on_leftpad_up_down
-def handle_leftpad_up_down(*args):
-    print('on_leftpad_up_down', *args)
-
-
-@gamepad.on_leftpad_up_up
-def handle_leftpad_up_up(*args):
-    print('on_leftpad_up_up', *args)
-
-
-@gamepad.on_leftpad_down_down
-def handle_leftpad_down_down(*args):
-    print('on_leftpad_down_down', *args)
-
-
-@gamepad.on_leftpad_down_up
-def handle_leftpad_down_up(*args):
-    print('on_leftpad_down_up', *args)
-
-
-for event in device.read_loop():
-    gamepad.handle_event(event)
+def main():
+    device = get_ps5_device()
+    gamepad = Gamepad()
+    motor_controller = motor.MotorController(
+        left_motor=motor.MotorConfig(
+            pin_forward=23,
+            pin_backward=24,
+            pin_en=25,
+        ),
+        right_motor=motor.MotorConfig(
+            pin_forward=7,
+            pin_backward=8,
+            pin_en=12,
+        ),
+        drum_motor=motor.MotorConfig(
+            pin_forward=20,
+            pin_backward=16,
+            pin_en=21,
+        ),
+    )
+
+    @gamepad.on_r2_analog
+    def handle_r2_analog(value):
+        r2_value = (value - 128) / 127 * 100
+        l2_value = (gamepad.l2_analog - 128) / 127 * 100
+        motor_controller.set_forward_backward(l2_value - r2_value)
+
+    @gamepad.on_l2_analog
+    def handle_l2_analog(value):
+        r2_value = (gamepad.r2_analog - 128) / 127 * 100
+        l2_value = (value - 128) / 127 * 100
+        motor_controller.set_forward_backward(l2_value - r2_value)
+
+    @gamepad.on_joy_l_x
+    def handle_joy_left_x(value):
+        value = (value - 128) / 127 * 100
+        motor_controller.set_steer(value)
+
+    @gamepad.on_l1_up
+    def handle_l1_up():
+        motor_controller.toggle_drum()
+
+    for event in device.read_loop():
+        gamepad.handle_event(event)
+
+
+if __name__ == '__main__':
+    main()
--- motor.py
+++ motor.py
@@ -25,6 +25,10 @@ class MotorController:
         self.__right_pwm = self.__setup_motor_pwm(self.__right_motor)
         self.__setup_motor(self.__drum_motor)
 
+        self._drum_on = False
+        self._forward_backward = 0.0
+        self._steer = 0.0
+
     def __del__(self):
         GPIO.cleanup()
 
@@ -32,16 +36,61 @@ class MotorController:
     def __setup_motor_pwm(motor):
         GPIO.setup(motor.pin_forward, GPIO.OUT)
         GPIO.setup(motor.pin_backward, GPIO.OUT)
-        GPIO.setup(motor.pin_enable, GPIO.OUT)
+        GPIO.setup(motor.pin_en, GPIO.OUT)
         GPIO.output(motor.pin_forward, GPIO.LOW)
         GPIO.output(motor.pin_backward, GPIO.LOW)
-        return GPIO.PWM(motor.enable, 1000)
+        pwm = GPIO.PWM(motor.pin_en, 1000)
+        pwm.start(0)
+        return pwm
 
     @staticmethod
     def __setup_motor(motor):
         GPIO.setup(motor.pin_forward, GPIO.OUT)
         GPIO.setup(motor.pin_backward, GPIO.OUT)
-        GPIO.setup(motor.pin_enable, GPIO.OUT)
+        GPIO.setup(motor.pin_en, GPIO.OUT)
         GPIO.output(motor.pin_forward, GPIO.LOW)
         GPIO.output(motor.pin_backward, GPIO.LOW)
         GPIO.output(motor.pin_backward, GPIO.HIGH)
+
+    def toggle_drum(self):
+        self._drum_on = not self._drum_on
+        print('drum_on', self._drum_on)
+        if self._drum_on:
+            GPIO.output(self.__drum_motor.pin_forward, GPIO.HIGH)
+            GPIO.output(self.__drum_motor.pin_backward, GPIO.LOW)
+        else:
+            GPIO.output(self.__drum_motor.pin_forward, GPIO.LOW)
+            GPIO.output(self.__drum_motor.pin_backward, GPIO.LOW)
+
+    def set_forward_backward(self, value: float):
+        print('forward_backward', value)
+        self._forward_backward = value
+        self.__update_left_right_motors()
+
+    def set_steer(self, value: float):
+        print('steer', value)
+        self._steer = value
+        self.__update_left_right_motors()
+
+    def __update_left_right_motors(self):
+        left = self._forward_backward + self._steer
+        right = self._forward_backward - self._steer
+
+        if left < 0:
+            GPIO.output(self.__left_motor.pin_forward, GPIO.HIGH)
+            GPIO.output(self.__left_motor.pin_backward, GPIO.LOW)
+        else:
+            GPIO.output(self.__left_motor.pin_forward, GPIO.LOW)
+            GPIO.output(self.__left_motor.pin_backward, GPIO.HIGH)
+
+        if right < 0:
+            GPIO.output(self.__right_motor.pin_forward, GPIO.HIGH)
+            GPIO.output(self.__right_motor.pin_backward, GPIO.LOW)
+        else:
+            GPIO.output(self.__right_motor.pin_forward, GPIO.LOW)
+            GPIO.output(self.__right_motor.pin_backward, GPIO.HIGH)
+
+        left = max(0, min(abs(left), 100))
+        right = max(0, min(abs(right), 100))
+        self.__left_pwm.ChangeDutyCycle(left)
+        self.__right_pwm.ChangeDutyCycle(right)
